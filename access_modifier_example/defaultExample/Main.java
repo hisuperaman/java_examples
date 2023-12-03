08692979de9a8745ad7ab8810ffb5a56aae1c749
theaman05 <theaman1805@gmail.com>
2023-12-03 20:59:23 +0530

    added default access modifier example

---
+++ access_modifier_example/defaultExample/Main.java
@@ -0,0 +1,7 @@
+package defaultExample;
+
+public class Main{
+    public static void main(String[] args){
+        myClass.myPrint();
+    }
+}
---
+++ access_modifier_example/defaultExample/myClass.java
@@ -0,0 +1,9 @@
+package defaultExample;
+
+class myClass{
+
+    static void myPrint(){
+        System.out.println("same package class method");
+    }
+
+}
